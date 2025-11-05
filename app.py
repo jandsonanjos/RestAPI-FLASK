7a6bfde6bae08cdfa2d69eff8f8186fec54f816c
root <root@DESKTOP-DL7AD53.localdomain>
2025-11-05 12:18:01 -0300

    add projeto

--- app.py
+++ app.py
@@ -4,19 +4,23 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class Users(Resource):
     def get(self):
         return {'message': 'users 1'}
+
 
 class User(Resource):
     def post(self):
         return {'message': 'CPF'}
 
     def get(self, cpf):
-        return {"message":"CPF"}
+        return {"message": "CPF"}
+
 
 api.add_resource(Users, '/users')
 api.add_resource(User, '/user', '/user/<string:cpf>')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
